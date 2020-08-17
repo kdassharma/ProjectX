78847c091cfb2839c7289f7d411a5d6fe1b9b251
rbehal <rahulbehal01@hotmail.com>
2020-08-17 13:41:20 -0400

    Added calc_ear code, TODO: Fix no rectangles appearing

---
+++ calc_ear.py
@@ -0,0 +1,83 @@
+# python calc_ear.py --shape-predictor shape_predictor_68_face_landmarks.dat
+
+from scipy.spatial import distance as dist
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+import argparse
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+	
+video_path = "R:\\Users\\Rahul\\Documents\\Programming\\10_1.mp4"
+
+# Construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+    help="path to facial landmark predictor")
+args = vars(ap.parse_args())
+
+# Load and initialize video
+print("[INFO] Loading video...")
+cap = cv2.VideoCapture(video_path)
+success = cap.read() 
+
+# Initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+# Grab the indexes of the facial landmarks for the left and right eye
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+print("Indices initialized")
+
+ears = []
+
+while success:
+    # read next frame
+    success, img = cap.read()
+    
+    if not success:
+        break;
+		
+    frame = imutils.resize(img, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+    # TODO: Detector is not detecting any rectangles
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        
+        shape = predictor(gray, rect)        
+        shape = face_utils.shape_to_np(shape)
+        
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+        print(ear)
+        ears.append(ear)
+		
